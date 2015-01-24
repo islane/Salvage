4af487c641aab9315cff64ef5f10c5f8f86aa180
Jesse Smith <jesse.smth09@gmail.com>
2015-01-24 09:34:24 -0500

    Battery added

---
+++ Salvage/Assets/Scripts/Battery.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battery : MonoBehaviour {
+	public int batteryHealth = 100;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 
+		if (Input.GetKey(KeyCode.A))
+		{
+			batteryHealth =-1;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			batteryHealth =-1;
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			batteryHealth =-1;
+		}
+	}
+}
