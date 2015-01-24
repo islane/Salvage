011d23faa1d8edd763af9a182b17970b75dc3e28
Jesse Smith <jesse.smth09@gmail.com>
2015-01-24 16:20:29 -0500

    New BatteryStation

---
+++ Scripts/BatterStation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatterStation : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+				if (other.gameObject.tag == "Player") {
+						Debug.Log ("Player is charged");
+			//GameObject go = (GameObject) other;
+						Robot robot = other.gameObject.GetComponent<Robot>();
+						robot.SetBatteryHealth (10000);
+				}
+		}
+}
--- Scripts/Robot.cs
+++ Scripts/Robot.cs
@@ -7,33 +7,47 @@ public class Robot : MonoBehaviour {
 
 	public float movement = 10.0f;
 	public float jump = 300.0f;
-
+	public int batteryHealth = 100;
 	public bool activated = false;
 
 	public bool current = false;
-
+	
 	// Use this for initialization
+	public void SetBatteryHealth(int health){
+		batteryHealth = health;
+	}
+	
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
 		if(current)
 		{
 			if (Input.GetKey(KeyCode.A))
 			{
 				rigidBody2d.AddForce(new Vector2(-movement, 0.0f));
+				batteryHealth -= 1;
+				print (batteryHealth);
 			}
 			if (Input.GetKey(KeyCode.D))
 			{
 				rigidBody2d.AddForce(new Vector2(movement, 0.0f));
+				batteryHealth -= 1;
 			}
 			if (Input.GetKeyDown(KeyCode.Space))
 			{
 				rigidBody2d.AddForce(new Vector2(0.0f, jump));
-				//Debug.Log ()
+				batteryHealth -= 1;
 			}
+
+			if (batteryHealth == 0){
+				Destroy(gameObject);
+			}
+
 		}
 	}
 }
