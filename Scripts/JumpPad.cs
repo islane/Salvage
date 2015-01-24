becb7e915def10b49c38896d454b2289474eec71
Adam Silcott <adsilcott@gmail.com>
2015-01-24 13:17:33 -0500

    Jump pad

---
+++ Scripts/JumpPad.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpPad : MonoBehaviour {
+
+	public float bounce = 500; 
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("Bounce!");
+		other.rigidbody2D.AddForce (new Vector2(0.0f, bounce));
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
