94291594103da47966236173afa67aa9b503edda
Jesse Smith <jesse.smth09@gmail.com>
2015-01-24 18:07:18 -0500

    Added WaterDrops

---
+++ Scripts/WaterDrops.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterDrops : MonoBehaviour {
+
+	public Rigidbody2D projectile;
+	void Update() {
+			Rigidbody2D clone;
+			clone = (Rigidbody2D)Instantiate(projectile, transform.position, transform.rotation);
+			clone.velocity = transform.TransformDirection(Vector3.down * 10);
+		}
+	}
