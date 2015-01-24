37cc25368beea1ee0d7fbee1d9a7888c543c0a35
Jesse Smith <jesse.smth09@gmail.com>
2015-01-24 09:41:54 -0500

    Added Double Jump and Small

---
+++ Salvage/Assets/Scripts/DoubleJumpRobot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoubleJumpRobot : Robot{
+	public float djump = 500.0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+		{
+			rigidBody2d.AddForce(new Vector2(-movement, 0.0f));
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			rigidBody2d.AddForce(new Vector2(movement, 0.0f));
+		}
+		if (Input.GetKeyDown(KeyCode.Space)){
+			rigidBody2d.AddForce(new Vector2(0.0f, djump));
+		}
+
+	}
+		}
+
+
