2360bd049504d302f066c22824b7821212b52724
Lucas Falkenstein <lfalkenstein9@gmail.com>
2015-01-24 21:11:54 -0500

    Made  a bunch of prefabs, added a few sounds files. you may need to manually associate the prefabs with projects manually so that the correct values are used (defaults or custom)

--- Scripts/DoubleJumpRobot.cs
+++ Scripts/DoubleJumpRobot.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class DoubleJumpRobot : Robot{
 	public float djump = 500.0f;
+	public Rigidbody2D rigidBody2D;
 	// Use this for initialization
 	void Start () {
 
@@ -20,9 +21,10 @@ public class DoubleJumpRobot : Robot{
 		}
 		if (Input.GetKeyDown(KeyCode.Space)){
 			rigidBody2d.AddForce(new Vector2(0.0f, djump));
+			audio.Play();
 		}
 
 	}
-		}
+}
 
 
