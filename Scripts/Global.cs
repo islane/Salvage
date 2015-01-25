d776bc276e0618b1ff1891416f223d0e2e7d00b4
Lucas Falkenstein <lfalkenstein9@gmail.com>
2015-01-24 22:29:29 -0500

    changes to global

--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class Global : MonoBehaviour {
 
 	public Robot currentRobot;
-
 	public Robot robotSmall;
 	public Robot robotJump;
+	public Robot robotPush;
 
 	//public 
 	// Use this for initialization
@@ -19,21 +19,27 @@ public class Global : MonoBehaviour {
 
 		if(Input.GetKeyDown (KeyCode.Alpha1))
 		{
-			if(robotSmall.activated)
+			if(!(robotSmall.activated))
 			{
 				currentRobot.current = false;
 				currentRobot = robotSmall;
 				currentRobot.current = true;
 			}
 		}
-
-		
 		if(Input.GetKeyDown (KeyCode.Alpha2))
 		{
-			if(robotJump.activated)
+			if(!(robotJump.activated))
 			{
 				currentRobot.current = false;
 				currentRobot = robotJump;
+				currentRobot.current = true;
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3))
+		{
+			if(!(robotPush.activated)){
+				currentRobot.current = false;
+				currentRobot = robotPush;
 				currentRobot.current = true;
 			}
 		}
