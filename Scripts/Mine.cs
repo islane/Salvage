a01b3b7d7c3d3071e4987506cad4282b5bc732fe
Jesse Smith <jesse.smth09@gmail.com>
2015-01-25 05:37:05 -0500

    Changed Mine

--- Scripts/Mine.cs
+++ Scripts/Mine.cs
@@ -2,10 +2,28 @@
 using System.Collections;
 
 public class Mine : MonoBehaviour {
+	public int damage = 10;
+	
 	public AudioClip audio;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
 	void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log ("Player hit the mine");
-		AudioSource.PlayClipAtPoint(audio, transform.position); 
-		Destroy(other);
+		
+		if (other.gameObject.GetComponent<Robot>())
+		{
+			other.gameObject.GetComponent<Robot>().Damage(damage);
+		}
+		
+		AudioSource.PlayClipAtPoint(audio, transform.position);
+		Destroy (gameObject);
+		//}
 	}
 }
