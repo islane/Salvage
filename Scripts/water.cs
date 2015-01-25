79a0ea57c8b0eabbf29a56d58ba9de29ace7974f
Jesse Smith <jesse.smth09@gmail.com>
2015-01-25 02:08:41 -0500

    Added Water collision with my map

---
+++ Scripts/water.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class water : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Player hit the mine"); 
+		Destroy(other);
+	}
+}
